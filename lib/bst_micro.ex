cf805e3a011307e2886a58b8f1fb454fa2cfbc1f
Christopher Acosta <chriz@live.dk>
2021-04-11 15:36:30 +0200

    Initial Mix project

---
+++ lib/bst_micro.ex
@@ -0,0 +1,18 @@
+defmodule BSTMicro do
+  @moduledoc """
+  Documentation for BSTMicro.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BSTMicro.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bst_micro_test.exs
@@ -0,0 +1,8 @@
+defmodule BSTMicroTest do
+  use ExUnit.Case
+  doctest BSTMicro
+
+  test "greets the world" do
+    assert BSTMicro.hello() == :world
+  end
+end
